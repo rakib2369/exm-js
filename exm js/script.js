0a26570b408c179c0fb5484671bc567ef3b2fe95
rakib2369 <144321730+rakib2369@users.noreply.github.com>
2023-10-22 13:33:18 +0600

    Add files via upload

---
+++ exm js/script.js
@@ -0,0 +1,114 @@
+// ans Number.1
+// var x =10;
+// var y = 15;
+// var result = x+y;
+// console.log(result);
+
+// ans Number.3
+// the varient of type:
+// 1.string. example: 'rakibul hasan'
+// 2.float. example: 14.4,15.54
+// 3.intiger. example: 5,6,7,8
+ 
+// ans number.4
+// var er nam koroner khetre j bisoi gulo kheyal rakhte hobe:
+// 1. var er nam ohbossoi unique hote hobe
+// 2. var er nam kono reserved key er nam hote parbe na 
+// 3. var er nam er majhe kono space thaka jabe nh
+// 4. var er namer pothom e kono numnber thaka jabe nh
+
+// ans number.5
+// var name = "rakibul hasan";
+// console.log(name)
+
+// ans Number.7
+// % (er maddhome vhagshes pawa jai)
+// ** (er maddhome power er man bosanu jai)
+// ++ (er maddhome var er man 1 1 kore increment hoi)
+// -- (er moddhome var er man 1 1 kore decrement hoi)
+
+// ans Number.6
+// arithmetic operator er kaj holo:
+// (+,-,*,/) er kaj kora
+ 
+// ans number.8
+// operator jader niye kaj kore tader k operand bole (5,7) eikhan e 5&7 operand
+
+// ans Number.9
+// var x = 10;
+// var y = 36;
+// var z = x+y;
+// console.log(z);
+
+// ans number.12
+// var math= 84;
+// var chemistry=88;
+// var physics=90;
+// var bangla=82;
+// var number_of_subject = 4;
+// var total_sum = math+chemistry+physics+bangla;
+// var avg = total_sum/number_of_subject;
+// console.log(avg);
+
+// ans Number.16
+// array er indrx '0'theke suru hoi
+
+// ans number.17
+// var numbers =[34,56,74,24,88,34,776,34,5]; 
+// numbers[5]= 334;
+// console.log(numbers);
+
+//  ans number.18
+// var numbers=[34,56,74,24,88,34,776,34,5];
+// numbers.push(234);
+// numbers.unshift(998);
+// console.log(numbers);
+
+// ans Number.21
+// var fruits= ["banana","orange","apple","mango"];
+// fruits.shift();
+// console.log(fruits)
+
+// ans number.22
+// var fruits=["banana","orange","apple","mango"];
+// fruits.slice(3,0,"lichi","pineapple","watermelon");
+// console.log(fruits);
+
+// ans Number.25
+// var pen_quantity=10;
+// var pen_total_price=185;
+// var price_of_single_pen= pen_total_price/pen_quantity;
+// var price_of_3_pen=price_of_single_pen*3;
+// console.log(price_of_3_pen);
+
+// ans Number.20
+// var team1=["rahim","karim"];
+// var team2=["tanim","biplop","tareq"];
+// var team3=["sajjat","arefin"];
+// console.log(team1.concat(team2,team3));
+
+// ans number.19
+// var numbers=[[1,2],[3,4],[5,6]];
+// console.log(numbers.flat());
+
+// ans Number.26
+
+// var input=250;
+// var per=20;
+// var ans=input*(per/100);
+// console.log(ans);
+
+// ans number.11
+// var input=8**3;
+// console.log(input);
+
+// ans Number.10
+// var x=375;
+// var y=8;
+// var z=x%y;
+// console.log(z);
+
+// ans number.27
+// var fruits=["banana","apple","mango"];
+// var result=fruits.indexOf("kiwi");
+// console.log(result);
